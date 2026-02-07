1e1bd4f9a2efb84676bd75e94a35559b2fa34f68
Yusuf-Yildiz34 <yyyusufyildiz3@gmail.com>
2026-02-07 10:05:48 +0300

    favicon made

---
+++ script.js
@@ -0,0 +1,81 @@
+const bir = document.getElementById('bir');
+const iki = document.getElementById('iki');
+const uc = document.querySelector('.uc');
+const dort = document.querySelector('.dort');
+
+if (iki) {
+        iki.addEventListener('click', () => {
+                localStorage.setItem("testType", "kilic");
+                window.open("originals.html", "_blank");
+        });
+}
+
+if (uc) {
+        uc.addEventListener('click', () => {
+                localStorage.setItem("testType", "ADC");
+                window.open("originals.html", "_blank");
+        });
+}
+
+if (dort) {
+        dort.addEventListener('click', () => {
+                localStorage.setItem("testType", "kurtlar");
+                window.open("originals.html", "_blank");
+        });
+}
+
+const testType = localStorage.getItem("testType");
+let ilktxtEl = document.querySelector('.ilktxt');
+
+if (ilktxtEl && testType === "kurtlar") {
+        ilktxtEl.innerText = ilktxtEl.innerText.replace(
+                /originals\s*characters/gi,
+                "Kurtlar Vadisi"
+        );
+}
+
+if (ilktxtEl && testType === "ADC") {
+        ilktxtEl.innerText = ilktxtEl.innerText.replace(
+                /originals\s*characters/gi,
+                "ADC"
+        );
+        localStorage.removeItem("testType");
+}
+
+if (ilktxtEl && testType === "kilic") {
+        ilktxtEl.innerText = ilktxtEl.innerText.replace(
+                /originals\s*characters/gi,
+                "KILIC"
+        );
+        localStorage.removeItem("testType");
+}
+
+const gcont = document.querySelectorAll('.gcont');
+if (gcont) {
+        gcont.forEach((element) => {
+                element.addEventListener('click', () => {
+                        window.open('originals.html', '_blank');
+                });
+        });
+           
+}
+const start = document.getElementById('start');
+const control = document.querySelector('.control');
+
+if (start && control) {
+        start.addEventListener('click', () => {
+                control.style.display = 'none';
+                const main = document.querySelector('.main');
+                if (main) {
+                        main.style.display = 'block';
+                }
+        });
+
+}
+
+if (iki) {
+        iki.addEventListener('click', () => {
+                window.open('originals.html', '_blank');
+        });
+
+}

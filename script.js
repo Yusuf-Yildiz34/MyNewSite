ac306da92f0901089d5e0d1994d4ffbf106cd835
Yusuf-Yildiz34 <yyyusufyildiz3@gmail.com>
2026-02-25 11:23:01 +0300

    two more test

--- script.js
+++ script.js
@@ -94,11 +94,11 @@ input.addEventListener("input", () => {
         property.textContent = input.value;
 });
 const names = [
-        "Amca", "Aurora", "Aya", "Babag", "Zenci Finn", "Cemile", "Caroline",
-        "Rebekah", "Dahlia", "Damon", "Davina", "Diego", "Elena", "Esther",
-        "Finn", "Freya", "Josh", "Hayley", "Jackson", "Kol", "Lucien", "Marcel",
-        "Mason", "Mikael", "Bonnie", "Niklauss", "Sırret Kadın", "Sage",
-        "Alaric Saltzman", "Stephan", "Tristan", "Tyler"
+        "Kieran O'Connella", "Aurora De Martel", "Aya", "Elijah Mikaelson", "Zenci Finn", "Camille O'Connella", "Caroline",
+        "Rebekah Mikaelson", "Dahlia", "Damon", "Davina", "Diego", "Elena", "Esther",
+        "Finn Mikaelson", "Freya Mikaelson", "Josh", "Hayley", "Jackson", "Kol", "Lucien Castle", "Marcel Gerard",
+        "Mason Lockwood", "Mikael Mikaelson", "Bonnie", "Klaus Mikaelson", "Katherine Pierce", "Sage",
+        "Alaric Saltzman", "Stephan Salvotore", "Tristan De Martel", "Tyler Lockwood"
 ];
 
 const chars = [];
@@ -109,278 +109,115 @@ for (let i = 1; i <= 32; i++) {
         });
 }
 
-
-let imgEl = document.querySelector("#img1");
+let imgEl1 = document.querySelector("#img1");
 let imgEl2 = document.querySelector("#img2");
-let img3 = document.querySelector("#img3");
 let nameEl1 = document.querySelector("#name1");
 let nameEl2 = document.querySelector("#name2");
-let tutucular = document.querySelectorAll('.tutucu');
-let resultEl = document.querySelector('.sonuc');
-let resultsoz = document.querySelector('#result');
-let tutucu1 = document.getElementById('tutucu1');
-let tutucu2 = document.getElementById('tutucu2');
-
+let tutucu1 = document.getElementById("tutucu1");
+let tutucu2 = document.getElementById("tutucu2");
+let tutucular = document.querySelectorAll(".tutucu");
 let yazi = document.querySelector("#yazi");
-let char1, char2;
+let next = document.querySelector(".next");
+
 let pool = [...chars];
-let pool2 = [];
-let pool3 = [];
-let pool4 = [];
-let pool5 = [];
-let winner;
+let nextRound = [];
+let currentIndex = 0;
+let leftChar, rightChar;
 
+shuffle(pool);
+starter();
 
-function random() {
-        
-        
-
-        const index1 = Math.floor(Math.random() * pool.length);
-        char1 = pool.splice(index1, 1)[0];
-        
-
-        const index2 = Math.floor(Math.random() * pool.length);
-        char2 = pool.splice(index2, 1)[0];
-        
-        
-       
-        imgEl.src = char1.img;
-        nameEl1.textContent = char1.name;
-        imgEl2.src = char2.img;
-        nameEl2.textContent = char2.name;
-
-    
+function shuffle(array) {
+        for (let i = array.length - 1; i > 0; i--) {
+                const j = Math.floor(Math.random() * (i + 1));
+                [array[i], array[j]] = [array[j], array[i]];
+        }
 }
-tutucu1.addEventListener('click', () => {
 
-        if (pool.length > 0) {
-                
-                random();
-        }
+function starter() {
 
-        else if (pool2.length > 0 && pool.length === 0  ) {
-                secTour();
-                pool3.push(char1);
-                random2();
+        if (pool.length === 1) {
+                showWinner(pool[0]);
+                return;
         }
 
-        else if (pool3.length > 0 && pool.length === 0 && pool2.length === 0) {
-                thirdTour();
-                pool4.push(char1);
-                random3();
+        if (currentIndex >= pool.length) {
+
+                pool = [...nextRound];
+                nextRound = [];
+                currentIndex = 0;
+
+                if (pool.length === 1) {
+                        showWinner(pool[0]);
+                        return;
+                }
+
+                shuffle(pool);
+                checkRound();
         }
 
-        else if (pool4.length > 0 && pool.length === 0 && pool2.length === 0 && pool3.length === 0) {
-                fourthTour();
-                pool5.push(char1);
-                random4();
-        }
-        else if (pool5.length > 0 && pool4.length === 0 && pool.length === 0 && pool2.length === 0 && pool3.length === 0) {
-                fifthTour();
-                resultEl = char1;
-               
-        }
+        leftChar = pool[currentIndex];
+        rightChar = pool[currentIndex + 1];
+
+        if (!rightChar) return; // ekstra güvenlik
+
+        imgEl1.src = leftChar.img;
+        nameEl1.textContent = leftChar.name;
+
+        imgEl2.src = rightChar.img;
+        nameEl2.textContent = rightChar.name;
+}
+
+
+tutucu1.addEventListener("click", () => {
+        nextRound.push(leftChar);
+        currentIndex += 2;
+        starter();
 });
 
-
-tutucu2.addEventListener('click', () => {
-
-        if (pool.length > 0) {
-                pool2.push(char2);
-                random();
-        }
-
-        else if (pool2.length > 0 && pool.length === 0  ) {
-                secTour();
-                pool3.push(char2);
-                random2();
-        }
-
-        else if (pool3.length > 0  && pool.length === 0 && pool2.length === 0) {
-                thirdTour();
-                pool4.push(char2);
-                random3();
-        }
-
-        else if (pool4.length > 0 && pool.length === 0 && pool2.length === 0 && pool3.length === 0) {
-                fourthTour();
-                pool5.push(char2);
-                random4();
-        }
-        else if (pool5.length > 0 && pool4.length === 0 && pool.length === 0 && pool2.length === 0 && pool3.length === 0){
-                fifthTour();
-                winner.textContent  = char2.name;
-                winner.src = char2.img;
-                
-        }
+tutucu2.addEventListener("click", () => {
+        nextRound.push(rightChar);
+        currentIndex += 2;
+        starter();
 });
 
+function checkRound() {
 
-
-
-function random2() {
-        
-
-        const index55 = Math.floor(Math.random() * pool2.length);
-        char1 = pool2.splice(index55, 1)[0];
-        
-
-        const index3 = Math.floor(Math.random() * pool2.length);
-        char2 = pool2.splice(index3, 1)[0];
-
-        imgEl.src = char1.img;
-        nameEl1.textContent = char1.name;
-        imgEl2.src = char2.img;
-        nameEl2.textContent = char2.name;
-
+        if (pool.length === 16) changeTour("İkinci Tur");
+        else if (pool.length === 8) changeTour("Çeyrek Final");
+        else if (pool.length === 4) changeTour("Yarı Final");
+        else if (pool.length === 2) changeTour("Final");
 }
 
-function random3() {
-        
+function changeTour(text) {
 
-        const index8 = Math.floor(Math.random() * pool3.length);
-        char1 = pool3.splice(index8, 1)[0];
+        next.innerText = text;
 
-        const index9 = Math.floor(Math.random() * pool3.length);
-        char2 = pool3.splice(index9, 1)[0];
+        tutucular.forEach(t => t.style.display = "none");
+        yazi.style.display = "none";
+        next.style.display = "block";
+        next.classList.add("show");
 
-        imgEl.src = char1.img;
-        nameEl1.textContent = char1.name;
-        imgEl2.src = char2.img;
-        nameEl2.textContent = char2.name;
+        setTimeout(() => {
+                next.classList.remove("show");
+                tutucular.forEach(t => t.style.display = "flex");
+                yazi.style.display = "block";
+        }, 1500);
 }
-function random4() {
+let sonuc = document.querySelector(".sonuc");
+let img3 = document.getElementById("img3");
+let result = document.getElementById("result");
 
-        if (pool4.length < 2) return;
-        const index12 = Math.floor(Math.random() * pool4.length);
-        char1 = pool4.splice(index12, 1)[0];
+function showWinner(winner) {
 
-        const index11 = Math.floor(Math.random() * pool4.length);
-        char2 = pool4.splice(index11, 1)[0];
+        tutucular.forEach(t => t.style.display = "none");
+        yazi.style.display = "none";
 
-        imgEl.src = char1.img;
-        nameEl1.textContent = char1.name;
-        imgEl2.src = char2.img;
-        nameEl2.textContent = char2.name;
+        sonuc.classList.add("show");
+
+        img3.src = winner.img;   
+        img3.load();            
+        img3.play().catch(() => { }); 
+        result.innerText = "WINNER: " + winner.name;
+        next.style.display = "none";
 }
-
- 
-function secTour() {
-        if (pool.length < 2) {
-                tutucular.forEach((tutucu) => {
-                        tutucu.style.display = 'none';
-                });
-                const next = document.querySelector('.next');
-                if (next && yazi) {
-                        next.style.display = 'block';
-                        yazi.style.display = 'none';
-                        next.classList.add('show');
-                        
-                        setTimeout(() => {
-                                next.classList.remove('show');
-                                tutucular.forEach((tutucu) => {
-                                        tutucu.style.display = 'flex';
-                                       
-                                });
-                                yazi.style.display = 'block';
-                            
-                        }, 1500);
-                }
-                secTour = function () { };
-
-}
-}
-function thirdTour() {
-      
-        if (pool2.length < 2) {
-                const next = document.querySelector('.next');
-               next.innerText = next.innerText.replace(
-                        /Second\s*Tour/gi,
-                        "Third Tour"
-                );
-                tutucular.forEach((tutucu) => {
-                        tutucu.style.display = 'none';
-                });
-                
-                if (next && yazi) {
-                        next.style.display = 'block';
-                        yazi.style.display = 'none';
-                        next.classList.add('show');
-
-                        setTimeout(() => {
-                                next.classList.remove('show');
-                                tutucular.forEach((tutucu) => {
-                                        tutucu.style.display = 'flex';
-
-                                });
-                                yazi.style.display = 'block';
-
-                        }, 1500);
-                }
-                thirdTour = function () { };
-
-        }
-}
-function fourthTour() {
-
-        if (pool3.length < 2) {
-                const next = document.querySelector('.next');
-                next.innerText = next.innerText.replace(
-                        /Second\s*Tour/gi,
-                        "Fourth Tour"
-                );
-                tutucular.forEach((tutucu) => {
-                        tutucu.style.display = 'none';
-                });
-
-                if (next && yazi) {
-                        next.style.display = 'block';
-                        yazi.style.display = 'none';
-                        next.classList.add('show');
-
-                        setTimeout(() => {
-                                next.classList.remove('show');
-                                tutucular.forEach((tutucu) => {
-                                        tutucu.style.display = 'flex';
-
-                                });
-                                yazi.style.display = 'block';
-
-                        }, 1500);
-                }
-                fourthTour = function () { };
-
-        }
-}
-function fifthTour() {
-
-        if (pool4.length < 2) {
-                const next = document.querySelector('.next');
-                next.innerText = next.innerText.replace(
-                        /Second\s*Tour/gi,
-                        "Fifth Tour"
-                );
-                tutucular.forEach((tutucu) => {
-                        tutucu.style.display = 'none';
-                });
-
-                if (next && yazi) {
-                        next.style.display = 'block';
-                        yazi.style.display = 'none';
-                        next.classList.add('show');
-
-                        setTimeout(() => {
-                                next.classList.remove('show');
-                                tutucular.forEach((tutucu) => {
-                                        tutucu.style.display = 'flex';
-
-                                });
-                                yazi.style.display = 'block';
-
-                        }, 1500);
-                }
-                fifthTour = function () { };
-
-        }
-}
-random();
